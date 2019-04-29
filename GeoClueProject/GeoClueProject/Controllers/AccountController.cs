10d690047abd86e5c1eace5af38a4d893828ce1e
JonasRing <jonas.hg.ring@gmail.com>
2019-04-29 22:09:54 +0200

    Register, Login/Logout feature added

--- GeoClueProject/GeoClueProject/Controllers/AccountController.cs
+++ GeoClueProject/GeoClueProject/Controllers/AccountController.cs
@@ -5,20 +5,90 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GeoClueProject.Models.ViewModels;
 using GeoClueProject.Models;
-
+using Microsoft.AspNetCore.Authorization;
 
 namespace GeoClueProject.Controllers
 {
     public class AccountController : Controller
     {
+        AccountService accountService;
+
+        public AccountController(AccountService accountService)
+        {
+            this.accountService = accountService;
+        }
+
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(AccountRegisterVM viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            // Try to register user
+            var result = await accountService.TryRegisterAsync(viewModel);
+            if (!result.Succeeded)
+            {
+                // Show error
+                ModelState.AddModelError(string.Empty, result.Errors.First().Description);
+                return View(viewModel);
+            }
+
+            // Redirect user
+            return RedirectToAction(nameof(Login));
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new AccountLoginVM { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(AccountLoginVM viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            // Check if credentials is valid (and set auth cookie)
+            var result = await accountService.TryLoginAsync(viewModel);
+            if (!result.Succeeded)
+            {
+                // Show error
+                ModelState.AddModelError(nameof(AccountLoginVM.Username), "Login failed");
+                return View(viewModel);
+            }
+
+            // Redirect user
+            if (string.IsNullOrWhiteSpace(viewModel.ReturnUrl))
+                return RedirectToAction(nameof(Welcome));
+            else
+                return Redirect(viewModel.ReturnUrl);
+        }
+
+        public IActionResult Welcome()
+        {
+            return View(new AccountWelcomeVM { Username = User.Identity.Name });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await accountService.LogoutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ GeoClueProject/GeoClueProject/Models/AccountService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeoClueProject.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace GeoClueProject.Models
+{
+    public class AccountService
+    {
+        UserManager<MyIdentityUser> userManager;
+        SignInManager<MyIdentityUser> signInManager;
+
+        public AccountService(
+            UserManager<MyIdentityUser> userManager,
+            SignInManager<MyIdentityUser> signInManager
+            )
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public async Task<IdentityResult> TryRegisterAsync(AccountRegisterVM viewModel)
+        {
+            return await userManager.CreateAsync(
+                new MyIdentityUser { UserName = viewModel.Username },
+          viewModel.Password);
+        }
+
+        public async Task<SignInResult> TryLoginAsync(AccountLoginVM viewModel)
+        {
+            // Try to sign user
+            return await signInManager.PasswordSignInAsync(
+                viewModel.Username,
+                viewModel.Password,
+                isPersistent: false,
+                lockoutOnFailure: false);
+        }
+
+        public async Task LogoutAsync() // asynchrona metoder som inte returnerar något har task istället för void. Behöver inte return 
+        {
+            // Try to sign user
+            await signInManager.SignOutAsync();
+        }
+    }
+}
