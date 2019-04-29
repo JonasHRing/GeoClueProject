ff442ed29534b9747aa41e1cecac9acda1890695
JonasRing <jonas.hg.ring@gmail.com>
2019-04-29 13:57:57 +0200

    MVC grunden

---
+++ GeoClueProject/GeoClueProject/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoClueProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Game/Singleplayer")]
+        public IActionResult Game()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return Content("Now you are at the login page");
+        }
+
+        public IActionResult Register()
+        {
+            return Content("Here is the page where you register");
+        }
+
+    }
+}
